0820e0ea827093e38a6d4c74c3f11b3ffb841964
Project M Engine <projectm@macrodroid.dev>
2026-09-07 12:42:00 -0300

    fix(theme): add missing MaterialTheme import in TabsTray

--- app/src/main/java/org/mozilla/fenix/tabstray/ui/tabstray/TabsTray.kt
+++ app/src/main/java/org/mozilla/fenix/tabstray/ui/tabstray/TabsTray.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
